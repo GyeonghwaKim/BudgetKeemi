7ef9c40482b60e6afc8379015e0676e0ea9a61af
GyeongHwa <gyeonghwaa_@naver.com>
2024-08-23 23:14:23 +0900

    first commit

---
+++ src/main/java/com/example/budgeKeemi/BudgeKeemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgeKeemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgeKeemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgeKeemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budgeKeemi/BudgeKeemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgeKeemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgeKeemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
