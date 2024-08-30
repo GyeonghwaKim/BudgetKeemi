5a098c4fafd0c480f716a7623a51204e0530ab3e
GyeongHwa <gyeonghwaa_@naver.com>
2024-08-31 01:20:29 +0900

    클래스 위치 변경

---
+++ src/main/java/com/example/budgeKeemi/dto/JoinForm.java
@@ -0,0 +1,23 @@
+package com.example.budgeKeemi.dto;
+
+
+import com.example.budgeKeemi.domain.Member;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class JoinForm {
+    private String username;
+    private String email;
+
+
+    public static Member toEntity(JoinForm joinForm) {
+        return Member.builder()
+                .username(joinForm.getUsername())
+                .email(joinForm.getEmail())
+                .joinDate(LocalDateTime.now())
+                .build();
+
+    }
+}
