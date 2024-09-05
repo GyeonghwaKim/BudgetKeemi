b0148a88992a790b332925cb759f816b020883d1
GyeongHwa <gyeonghwaa_@naver.com>
2024-09-05 15:01:46 +0900

    accout type select

--- src/main/java/com/example/budgeKeemi/dto/RespTransaction.java
+++ src/main/java/com/example/budgeKeemi/dto/RespTransaction.java
@@ -11,26 +11,27 @@ public class RespTransaction {
 
     private Long id;
     private Long accountId;
-    private Long categoryId;
+    private String categoryName;
     private int amount;
     private LocalDateTime transacDate;
     private String description;
 
     @Builder
-    public RespTransaction(Long id, Long accountId, Long categoryId, int amount, LocalDateTime transacDate, String description) {
+    public RespTransaction(Long id, Long accountId, String categoryName, int amount, LocalDateTime transacDate, String description) {
         this.id = id;
         this.accountId = accountId;
-        this.categoryId = categoryId;
+        this.categoryName = categoryName;
         this.amount = amount;
         this.transacDate = transacDate;
         this.description = description;
     }
 
+
+
     public static RespTransaction toDto(Transaction transaction){
         return RespTransaction.builder()
                 .id(transaction.getId())
-        //        .accountId(transaction.getAccount().getId())
-        //        .categoryId(transaction.getCategory().getId())
+                .categoryName(transaction.getCategory().getName())
                 .amount(transaction.getAmount())
                 .transacDate(transaction.getTransacDate())
                 .description(transaction.getDescription())
